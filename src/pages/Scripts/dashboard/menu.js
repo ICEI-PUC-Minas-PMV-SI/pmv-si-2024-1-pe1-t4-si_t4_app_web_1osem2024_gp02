55fe93182d83b3010cbe7ca7a72a7ef38c34cb52
Morgana Larissa <morganalrsantos@gmail.com>
2024-04-25 21:59:27 -0300

    feat: adicionei alterações na sidebar

--- src/pages/Scripts/dashboard/menu.js
+++ src/pages/Scripts/dashboard/menu.js
@@ -1,18 +1,54 @@
-function openNav() {
-  document.getElementById("Sidebar").style.display = "flex";
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  const aside = document.getElementById("Sidebar");
-  const menuButton = document.querySelector(".menu");
+  var sidebar = document.getElementById("Sidebar");
+  var menuButton = document.querySelector(".menu");
+  var buttons = document.querySelectorAll('.menu-btn');
+  var sections = {
+    'resumoBtn': 'resumoSection',
+    'transacoesBtn': 'transacoesSection',
+    'metasBtn': 'metasSection'
+  };
+  var conteudoPrincipal = document.getElementById('conteudoPrincipal');
 
-  // Fechar barra lateral quando clicar fora dela
-  window.addEventListener("click", function (event) {
-    // Verificar se a tela é pequena
-    if (window.innerWidth <= 768) {
-      if (!aside.contains(event.target) && !menuButton.contains(event.target)) {
-        aside.style.display = "none";
-      }
+  function toggleSection(buttonId) {
+    // Esconde todas as seções
+    Object.keys(sections).forEach(function(key) {
+      document.getElementById(sections[key]).classList.add('hidden');
+    });
+    // Mostra a seção relacionada ao botão
+    if (document.getElementById(sections[buttonId]).classList.contains('hidden')) {
+      document.getElementById(sections[buttonId]).classList.remove('hidden');
+      conteudoPrincipal.classList.add('hidden');
+    } else {
+      conteudoPrincipal.classList.remove('hidden');
+    }
+  }
+
+  function activateButton(button) {
+    // Desativa todos os botões
+    buttons.forEach(function(btn) {
+      btn.classList.remove('green-button');
+    });
+    // Ativa o botão pressionado
+    button.classList.add('green-button');
+  }
+
+  // Adiciona eventos para os botões do menu
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      toggleSection(this.id);
+      activateButton(this);
+    });
+  });
+
+  // Evento de clique para mostrar/ocultar sidebar
+  menuButton.addEventListener('click', function() {
+    sidebar.style.display = (sidebar.style.display === 'flex') ? 'none' : 'flex';
+  });
+
+  // Evento para fechar a sidebar ao clicar fora
+  window.addEventListener('click', function(event) {
+    if (window.innerWidth <= 768 && !sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+      sidebar.style.display = 'none';
     }
   });
 });
