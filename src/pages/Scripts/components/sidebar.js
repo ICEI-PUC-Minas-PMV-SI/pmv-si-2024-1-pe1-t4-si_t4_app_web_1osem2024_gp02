367ee3c148ae2f8460a64c3e17a2dc1e5abb9d57
linuxpedro <pdro.dev97@gmail.com>
2024-05-02 19:29:50 -0300

    fix: import dashboard sidebar

--- src/pages/Scripts/components/sidebar.js
+++ src/pages/Scripts/components/sidebar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('sidebar.html')
+    fetch('/src/pages/components/sidebar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('sidebarContainer').innerHTML = data;
