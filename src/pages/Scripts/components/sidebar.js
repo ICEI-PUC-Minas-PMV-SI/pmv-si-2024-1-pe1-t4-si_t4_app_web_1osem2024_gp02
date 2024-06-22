f2dcb2f31667a8fd4fb6a17b7394f0680c8737fa
linuxpedro <pdro.dev97@gmail.com>
2024-06-22 09:56:10 -0300

    feat: sidebar btn color

--- src/pages/Scripts/components/sidebar.js
+++ src/pages/Scripts/components/sidebar.js
@@ -3,5 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(response => response.text())
     .then(data => {
         document.getElementById('sidebarContainer').innerHTML = data;
+    })
+    .then(data => {
+        const path = window.location.pathname;
+
+            const pathToButtonId = {
+                "/src/pages/dashboard/": "resumoBtn",
+                "/src/pages/transacoes/": "transacoesBtn",
+                "/src/pages/metas/": "metasBtn"
+            };
+
+            if (pathToButtonId) {
+                const activeButtonId = pathToButtonId[path];
+
+                document.getElementById(activeButtonId).style.backgroundColor = "green";
+            }
     });
 });
